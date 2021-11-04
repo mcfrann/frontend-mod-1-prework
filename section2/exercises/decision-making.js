d0375a37b792238a801ee3330a3958fe97375c42
Francesca McConnell <fmcconnell59@gmail.com>
2021-11-04 12:56:29 -0600

    Save decision-making.js

--- section2/exercises/decision-making.js
+++ section2/exercises/decision-making.js
@@ -7,7 +7,7 @@ Spend some time changing the variables and running the file to see how the story
 
 var doorChoice = 1;
 var bearClothing = "";
-var bearChoice = 1;
+var bearChoice = 3;
 
 console.log("You enter a dark room with two doors. Do you go through #1 or #2?");
 
@@ -38,16 +38,27 @@ if (bearChoice === 1) {
 Questions
 
 1. In English, using technical vocabulary, describe what is happening between lines 14 and 18.
+This is an if else statment; the condition (doorChoice === 2) is being evaluated
+as true or false. The following blocks of code assign which clothing type will be
+used in the story if the condition is true (hat) or false (scarf).
 
 2. What variable has a new value assigned to it after the first if statement executes?
+bearClothing is the variable that has a new value assigned to it.
 
 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
+It would be a scarf because only door choice 1 would result in a hat.
 
 4. In English, using technical vocabulary, describe what is happening between lines lines 27 and 35.
+Else if statements are happening between these lines, listing the different story endings depending
+on the bearChoice value.
 
 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
+The final outcome for bearChoice equalling 3 would be "You run as fast as you can into
+the next room. It's full of snakes!"
 
 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
+The final outcome would be "You tell the bear the hat is too small and it starts to cry!"
 
 7. What is your favorite ending?
+My favorite ending is obviously the one where neither snakes nor a crying bear are involved, so the first one.
 */
