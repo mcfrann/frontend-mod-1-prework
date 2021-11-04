56e5754df14776101a8a6b919a012c6d10154d62
Francesca McConnell <fmcconnell59@gmail.com>
2021-11-04 12:57:35 -0600

    Save functions.js

--- section2/exercises/functions.js
+++ section2/exercises/functions.js
@@ -15,9 +15,9 @@ Make sure to run the file with node in your command line.
 // EXAMPLE: Write a function below that, when called will log "Severus Snape" to the console
 function printName() {
   console.log("Severus Snape");
-};
+};  /* Does this semicolon need to be here? I haven't seen it in the other texts*/
 
-printName(); 
+printName();
 printName();
 printName();
 printName();
@@ -26,6 +26,14 @@ printName();
 
 // YOU DO: Write a function named sayHello that logs to the console "Oh, Hello!"
 // Then, call the function 2 times.
+
+function sayHello() {
+  console.log ('Oh, Hello!');
+}
+
+sayHello();
+sayHello();
+
 
 //-------------------
 // PART 2: Arguments and Parameters
@@ -36,17 +44,39 @@ printName();
 // Then, call the function 3 times, each time, passing it a different name.
 
 
+function greetMe(name) {
+  console.log(name);
+}
+
+greetMe("Francesca");
+greetMe("Henry");
+greetMe("Jenny");
+
+//greetMe();
 
 // YOU DO: Write a function that takes in 2 numbers as arguments, Numbers, and logs their sum
 // Then, call that function 3 times, each time, passing in 2 different Numbers.
 
+function total(weight, girth) {
+  console.log(weight + girth);
+}
+
+total(2, 7);
+total(3, 4);
+total(16, 23);
 
 
 // YOU DO🎈: Write a function that takes in two strings and prints a concatenation
 // of those two strings, for example the arguments could be ("Oscar", "Ruck") and
 // the end result might be "Oscar and Ruck are BFFS".  Then, call that function.
 
+function lazyAfternoon(buddy, snack) {
+  console.log(buddy + " is hungry for a " + snack + ".");
+}
 
+lazyAfternoon("Francesca", "donut");
+lazyAfternoon("Anneke", "tortilla");
+lazyAfternoon("Sarah", "poptart");
 
 //-------------------
 // PART 3: Naming is Hard
@@ -60,6 +90,10 @@ Look at the code you wrote for the previous YOU DO🎈 - what did you name the f
 What did you name each parameter, and why?
 
 EXPLAIN:
+
+I named the function lazyAfternoon because I wanted it to describe the context of
+the statment I'm logging. The parameters were buddy and snack, to log which buddy
+was hungry for which snack on our lazy afternoon together.
 
 
 */
