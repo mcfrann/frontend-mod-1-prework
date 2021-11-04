d47063fd51b49d8c06116f41e28a4c5986d5aac4
Francesca McConnell <fmcconnell59@gmail.com>
2021-11-04 12:58:23 -0600

    Save if-statements.js

--- section2/exercises/if-statements.js
+++ section2/exercises/if-statements.js
@@ -12,7 +12,7 @@ Make sure to run the file with node in your command line.
 // EXAMPLE:Manipulate the variable 'weather' to see if you can print something other
   // than 'coat'.
 
-var weather = 'snowy';
+var weather = 'sunny';
 // var weather = 'icy';
 // var weather = 'rainy';
 // var weather = 'sunny';
@@ -31,10 +31,10 @@ if (weather == 'sunny') {
   console.log('good to go!');
 }
 
-/* 
+/*
 YOU DO:
-Using the dogAge variable defined below, 
-determine if a dog is a puppy (2 or younger), 
+Using the dogAge variable defined below,
+determine if a dog is a puppy (2 or younger),
 an adult, or elderly (10 or older).
 
 Log to the console the appropriate age range
@@ -42,7 +42,16 @@ Log to the console the appropriate age range
 */
 
 var dogAge = 3;
-// Write your conditional here
+// Write the conditional statement here.
+// If the dog is 3, then it is an adult.
+
+if (dogAge <= 2) {
+  console.log("It's a puppy!");
+} else if (dogAge >= 10) {
+  console.log("It's elderly... :(");
+} else {
+  console.log("It's an adult!");
+}
 
 
 /*
@@ -65,8 +74,12 @@ When numQuarters = 3, program should log "I have enough money for a gumball"
 var numQuarters = 0;
 
 // Write your conditional here
-console.log("I have enough money for a gumball");
-console.log("I don't have enough money for a gumball");
+
+if (numQuarters >= 2) {
+  console.log("I have enough money for a gumball");
+} else {
+  console.log("I don't have enough money for a gumball");
+}
 
 
 /*
@@ -82,15 +95,24 @@ When cupsOfFlour = 2 and hasSauce = true, your program should log "I can make pi
 When cupsOfFlour = 3 and hasSauce = true, your program should log "I can make pizza";
 */
 
-var cupsOfFlour = 1;
+var cupsOfFlour = 3;
 var hasSauce = true;
 
 // Write your conditional here
 
+if (cupsOfFlour >= 2 && hasSauce == true) {
+  console.log("I can make pizza");
+} else {
+  console.log("I cannot make pizza");
+}
+
 
 /*
-For the last two exercises, an ideal solution probably uses a logical operator. 
+For the last two exercises, an ideal solution probably uses a logical operator.
 Did yours? Do you know what a logical operator in JavaScript is? Google it to answer
 for yourself!
+
+Question: Why did the exercise before the last one need a logical operator? I didn't
+find that to be the case but maybe I did it wrong...??
 
 */
